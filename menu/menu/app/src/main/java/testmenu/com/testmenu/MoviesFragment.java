2039c2897259ff72e5cfda0ad41140408cbd4439
saeed <ssv1368@yahoo.com>
2017-09-25 10:59:02 +0330

    final

---
+++ menu/menu/app/src/main/java/testmenu/com/testmenu/MoviesFragment.java
@@ -0,0 +1,26 @@
+package testmenu.com.testmenu;
+
+/**
+ * Created by Administrator on 9/3/2017.
+ */
+
+
+//        import info.androidhive.tabsswipe.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MoviesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        return rootView;
+    }
+}
